050ce4bff6c622e989465c5e3a0f41c1de7c1240
Alex Benfaremo <benfa.alex@gmail.com>
2016-05-30 15:10:20 +0200

    Update shutdown.py to work without ApioOS

--- shutdown.py
+++ shutdown.py
@@ -2,20 +2,13 @@
 import RPi.GPIO as GPIO
 import os
 import time
-import serial
-import urllib2
 
 GPIO.setmode(GPIO.BCM)
 PIN_INPUT = 22
 PIN_INPUT2 = 23
-#GPIO.setup(PIN_INPUT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(PIN_INPUT, GPIO.OUT)
 GPIO.setup(PIN_INPUT2, GPIO.IN)
 GPIO.output(PIN_INPUT,True)
-ser = serial.Serial("/dev/ttyACM0", 115200)
-time.sleep(1)
-ser.write("l99:reset:-")
-
 
 while(True):
 	READ = GPIO.input(PIN_INPUT2)
@@ -25,7 +18,8 @@ while(True):
 	print "!!!Pin 22: "
 	print READ1
 	if READ == False:
-			#ATTIVA LA PROCEDURA DI SPEGNIMENTO
-		urllib2.urlopen("http://localhost:8083/apio/shutdown").read()
+		#ATTIVA LA PROCEDURA DI SPEGNIMENTO
+		shutdown_time = 5 #Max 30
+		time.sleep(shutdown_time)
 		GPIO.output(PIN_INPUT,False)
-		time.sleep(5)
+		os.system("shutdown -h now")
